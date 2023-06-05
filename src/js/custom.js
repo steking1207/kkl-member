405ab73107ca77a8e46882099138ea5643dcde23
steking1207 <steking1207@gmail.com>
2023-06-05 22:23:41 +0800

    230605: first update

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,154 +1,70 @@
-const swiperKv = new Swiper(".swiper-kv", {
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 5000,
-    },
-    loop: true,
-});
-const swiperProd = new Swiper(".swiper-product", {
-    // spaceBetween: 24,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 2,
-            slidesPerColumn: 6,
-            slidesPerColumnFill: 'row',
-            watchOverflow: true,
-        },
-        1024: {
-            slidesPerView: 4,
-            slidesPerGroup: 4,
-            slidesPerColumn: 1,
-        },
-    },
-});
-const swiperBadge = new Swiper(".swiper-badge", {
-    spaceBetween: 24,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 3,
-            slidesPerColumn: 3,
-            slidesPerGroup: 3,
-            slidesPerColumnFill: 'row',
-            watchOverflow: true,
-        },
-        1024: {
-            slidesPerView: 5,
-            slidesPerGroup: 5,
-            slidesPerColumn: 3,
-            slidesPerColumnFill: 'row',
-            watchOverflow: true,
-        },
-    },
-});
-const swiperClass = new Swiper(".swiper-class", {
-    spaceBetween: 24,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-            slidesPerGroup: 1,
-            slidesPerColumn: 1,
-        },
-        1024: {
-            slidesPerView: 3,
-            slidesPerGroup: 3,
-            slidesPerColumn: 1,
-        },
-    },
+$('#google-form').submit(function (e) {  
+  e.preventDefault();
+
+  if ($('#name').val() && $('#nation').val() && $('#phone').val() && $('#email').val()) {//需要先確認必填欄位是否填寫
+    $.ajax({
+      // url為Google Form按下submit的aciotn
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSc0zn-HrwxEEm6BPh1lCcRgQo5-0KwYDmFS1nip57UbW9ppHQ/formResponse",
+      crossDomain: true,//解決跨網域CORS的問題
+      data: {// entry.xxxxx 這些需要填寫您表單裡面的值，與其相互對應
+        "entry.902173817": $('#name').val(),
+        "entry.913999100": $('#title').val(),
+        "entry.2081263291": $('#nation').val(),
+        "entry.1916522837": $('#phone').val(),
+        "entry.1685011723": $('#address').val(),
+        "entry.1253561105": $('#email').val(),
+        "entry.2015907215": $('#idnumber').val()
+      },
+      type: "POST", //因為是要進行insert的動作，故事用POST
+      dataType: "JSONP",
+      complete: function () {
+        //完成後把這些欄位清空
+        $('#name').val('')
+        $('#title').val('')
+        $('#nation').val('')
+        $('#phone').val('')
+        $('#address').val('')
+        $('#email').val('')
+        $('#idnumber').val('')
+        //最後跳轉到感謝的頁面
+        window.location.replace("success.html");
+      }
+    });
+  }
 });
 
-var clicked = 0;
-$(".toggle-password").click(function (e) {
-    e.preventDefault();
-    $(this).toggleClass("toggle-password");
-      if (clicked == 0) {
-        $(this).html('<i class="bi bi-eye-slash"></i>');
-        clicked = 1;
-      } else {
-        $(this).html('<i class="bi bi-eye-fill"></i>');
-        clicked = 0;
-    }
-    var input = $($(this).attr("toggle"));
-    if (input.attr("type") == "password") {
-       input.attr("type", "text");
+var input = document.querySelector("#phone");
+  window.intlTelInput(input, {
+    initialCountry: "auto",
+    showFlags: "false",
+    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+    
+});
+
+function checkAge() {
+    var birthdateInput = document.getElementById('birthdate');
+    var birthDate = birthdateInput.value;
+    
+    // 取得今天的日期
+    var today = new Date();
+    
+    // 取得出生日期
+    var birth = new Date(birthDate);
+    
+    // 計算年齡
+    var age = today.getFullYear() - birth.getFullYear();
+    
+    // 檢查是否已經過了生日
+    var hasPassedBirthday = today.getMonth() > birth.getMonth() ||
+                            (today.getMonth() === birth.getMonth() && today.getDate() >= birth.getDate());
+    
+    // 檢查年齡是否大於等於18歲
+    if (age > 18 || (age === 18 && hasPassedBirthday)) {
+      window.location.href = "form.html";
     } else {
-       input.attr("type", "password");
+      window.location.href = "alert.html";
     }
-});
-
-$(document).ready(function() {
-
-    $('#sidebar').hcSticky({
-        // innerSticker: '.ad',
-        followScroll: false,
-        top:120,
-    });
-    
-    $(".form-select").change(function() {
-        $(this).find("option:selected").each(function() {
-            var optionValue = $(this).attr("value");
-            if (optionValue) {
-              $(".select-change").not("." + optionValue).hide();
-              $("." + optionValue).show();
-            } else {
-            //   $(".select-change").hide();
-            }
-        });
-    }).change();
-});
-
-$(function () {
-
-    $('.btn-anchor').click(function(){
-		$('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top -120
-    }, 500);
-		return false;
-	});
-
-    // js:ofi
-    objectFitImages('img.of-cover');
-    $('img.of-cover').css('opacity',1);
-
-    //banner
-    $('.btn-banner-close').click(function() {
-        $('#banner').fadeOut();
-    })
-    
-    $('.btn-mobile-menu').on('click', function(event) {
-        event.preventDefault();
-        $('.btn-mobile-menu').toggleClass('active');
-        $("body").toggleClass('scroll-hidden');
-    });
-    
-    $(".input-otp").keyup(function () {
-        if (this.value.length == this.maxLength) {
-          var $next = $(this).next('.input-otp');
-          if ($next.length)
-              $(this).next('.input-otp').focus();
-          else
-              $(this).blur();
-        }
-    });
-
-});
-
-function changeFavorite(anchor) {
-    var icon = anchor.querySelector("i");
-    icon.classList.toggle('bi-heart');
-    icon.classList.toggle('bi-heart-fill');
+}
+function closeWindow() {
+    window.close();
 }
